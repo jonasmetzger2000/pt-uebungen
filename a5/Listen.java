1526c8cd00f1955565a5cb65736d85f0b8fbf06a
Kerim Vurelli <kerim.vurelli@haw-hamburg.de>
2022-12-04 16:57:16 +0100

    aufgaben teilweise gelöst, rest morgen

--- a5/Listen.java
+++ a5/Listen.java
@@ -13,19 +13,47 @@ public class Listen {
     }
 
     public static List<Integer> collectAllIndices(List<String> ls, String elem) {
-        return null;
+        List<Integer> indices = new ArrayList<>();
+        for(int i=0; i<ls.size();i++){  //size gibt die Länge der Liste wieder
+            String vergleich = ls.get(i); //get holt den aktuellen i und weist es vergleich zu
+            if(elem.equals(vergleich)){
+                indices.add(i);
+            }
+        }
+        return indices;
     }
 
     public static void printOpposites(List<Integer> li) {
 
+        if(li.size()%2!=0){
+            System.out.println("Uneven List");
+        } else{
+            for(int i=0; i<li.size()/2 ;i++){
+                int vorne = li.get(i);
+                int hinten= li.get(li.size()-i-1);
+                System.out.printf("%d:%d %n",vorne,hinten);
+            }
+        }
     }
 
     public static boolean isProperSuperset(List<Integer> li1, List<Integer> li2) {
-        return false;
+        for(int i :li2){
+            if(!li1.contains(li2) || li2.size()>=li1.size()){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void removeAll(List<Object> lo, Object o) {
-
+        Iterator<Object> objectIterator = lo.iterator();
+            while(objectIterator.hasNext()){
+                Object compare = objectIterator.next();
+                if (compare.equals(o)){
+                    objectIterator.remove();
+                }
+            }
+        System.out.println(lo);
     }
 
     public static void removeX(List<Object> lo, int start, int end) {
